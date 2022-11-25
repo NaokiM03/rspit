9eeeaca0196b1a7c1ae366309ec46a9d5a74cc7e
NaokiM03 <naokim03.lib+dev@gmail.com>
2022-11-25 20:34:30 +0900

    Add test to separate front matter and code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn project_from() {
+    const INPUT: &str = r#"
+//# [package]
+//# name = "test"
+//# version = "0.1.0"
+//# edition = "2021"
+//#
+//# [dependencies]
+//# rand = "*"
+
+use rand::prelude::*;
+
+fn main() {
+    let num: u64 = random();
+    println!("num: {}", num);
+}
+"#;
+
+    const TOML: &str = r#"
+[package]
+name = "test"
+version = "0.1.0"
+edition = "2021"
+
+[dependencies]
+rand = "*"
+"#;
+
+    const SRC: &str = r#"
+use rand::prelude::*;
+
+fn main() {
+    let num: u64 = random();
+    println!("num: {}", num);
+}
+"#;
+
+    let project = Project::from(INPUT);
+    assert_eq!(project.toml, TOML.trim());
+    assert_eq!(project.src, SRC.trim());
+}

55ba5739b76855c468f6295c59188cad86cf6518
NaokiM03 <naokim03.lib+dev@gmail.com>
2022-11-29 00:58:33 +0900

    fmt

--- src/commands.rs
+++ src/commands.rs
@@ -1,15 +1,15 @@
 mod add;
 mod build;
 mod clean;
+mod init;
 mod list;
 mod release;
 mod run;
-mod init;
 
 pub(crate) use add::add_package;
 pub(crate) use build::{build_all, build_specified_package};
 pub(crate) use clean::clean_cache_dir;
+pub(crate) use init::init_snippet;
 pub(crate) use list::list_packages;
 pub(crate) use release::{release_all, release_specified_package};
 pub(crate) use run::{run_all, run_specified_package};
-pub(crate) use init::init_snippet;
